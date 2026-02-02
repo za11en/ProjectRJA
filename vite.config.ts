da90d4981986a0828732191c8d698e30cf871f7c
za11en <118923639+za11en@users.noreply.github.com>
2026-02-02 02:56:46 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
-  base: "/YOUR-REPO-NAME/",  // <--- ADD THIS LINE (Keep the slashes!)
+  base: "/ProjectRJA/",  // <--- ADD THIS LINE (Keep the slashes!)
   plugins: [react()],
   // ... keep the rest exactly as it is
 })
