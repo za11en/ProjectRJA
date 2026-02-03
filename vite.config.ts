e182788044c4c700124ffae59e86705a6b302d91
za11en <118923639+za11en@users.noreply.github.com>
2026-02-02 23:26:32 -0500

    Refactor vite.config.ts for cleaner environment loading
    Removed comments and adjusted configuration for environment variables.

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,11 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// We must use the function syntax (({ mode }) =>) to load environment variables
 export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    base: "/ProjectRJA/", 
+    
     plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
